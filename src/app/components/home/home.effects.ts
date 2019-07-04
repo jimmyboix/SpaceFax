c833472d33cce14c3ded81b80e8bb7f90f95c246
Jimmy Hendrie <james.hendrie@cgi.com>
2019-07-04 12:28:51 +0100

    trying to add versioning on app build

--- src/app/components/home/home.effects.ts
+++ src/app/components/home/home.effects.ts
@@ -3,7 +3,7 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, catchError, switchMap } from 'rxjs/operators';
 import { HomeService } from '../../services/home.service';
-import { getNextLaunch, getNextLaunchSuccess, EHomeActions, getNextLaunchFailure } from './home.actions';
+import { GetNextLaunch, GetNextLaunchSuccess, EHomeActions, GetNextLaunchFailure } from './home.actions';
 import { Store } from '@ngrx/store';
 import { IAppState } from 'src/app/app.state';
 import { Launch } from './home.component';
@@ -13,16 +13,16 @@ export class HomeEffects {
 
     @Effect()
     getNextLaunch$ = this.actions$.pipe(
-        ofType<getNextLaunch>(EHomeActions.GET_NEXT_LAUNCH),
+        ofType<GetNextLaunch>(EHomeActions.GET_NEXT_LAUNCH),
         switchMap(() => this.homeService.getNextLaunch()),
         map((nextLaunch: Launch) => {
             if (nextLaunch != null) {
-                return new getNextLaunchSuccess(nextLaunch);
+                return new GetNextLaunchSuccess(nextLaunch);
             } else {
                 throw new Error('Data from API is null');
             }
         }),
-        catchError((err: Error) => of(new getNextLaunchFailure(err)))
+        catchError((err: Error) => of(new GetNextLaunchFailure(err)))
     );
 
 
